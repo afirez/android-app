8d2e2cafcd9e8c185d049e40942a45b7545208a7
afirez <afirez@163.com>
2018-03-19 18:52:19 +0800

    video player by MediaCodec

--- app-video-player/src/main/java/com/example/videoplayer/MainActivity.java
+++ app-video-player/src/main/java/com/example/videoplayer/MainActivity.java
@@ -1,27 +1,75 @@
 package com.example.videoplayer;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView vvVideo;
+    private SurfaceView svVideo;
+    private String filePath;
+    private SimplePlayer player;
+    private TextView tvPlay;
+    private TextView tvPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vvVideo = (VideoView) findViewById(R.id.vp_vv_video);
-        // local source
-        //vvVideo.setVideoPath("");
-        // cloud source
-        vvVideo.setVideoURI(Uri.parse(""));
-
-        MediaController controller = new MediaController(this);
-        vvVideo.setMediaController(controller);
-        controller.setMediaPlayer(vvVideo);
+        svVideo = (SurfaceView) findViewById(R.id.vp_sv_video);
+        tvPlay = (TextView) findViewById(R.id.vp_tv_play);
+        tvPause = (TextView) findViewById(R.id.vp_tv_pause);
+        svVideo.getHolder().addCallback(callback);
+        tvPlay.setOnClickListener(playOnClickListener);
+        tvPause.setOnClickListener(pauseOnClickListener);
     }
+
+    private View.OnClickListener pauseOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (player != null) {
+                player.pause();
+            }
+        }
+    };
+
+    private View.OnClickListener playOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (player != null) {
+                player.continuePlay();
+            }
+        }
+    };
+
+    private SimplePlayer.IPlayStateListener playStateListener = new SimplePlayer.IPlayStateListener() {
+        @Override
+        public void videoAspect(int width, int height, float time) {
+
+        }
+    };
+
+    private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            player = new SimplePlayer(holder.getSurface(), filePath);
+            player.setPlayStateListener(playStateListener);
+            player.play();
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            if (player != null) {
+                player.destroy();
+            }
+        }
+    };
 }
