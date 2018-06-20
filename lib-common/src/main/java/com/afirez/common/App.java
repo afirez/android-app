7840f2af22986ad17d17d25ead92e7253e257b82
afirez <afirez@163.com>
2018-06-20 14:02:23 +0800

    [lib-common]

---
+++ lib-common/src/main/java/com/afirez/common/App.java
@@ -0,0 +1,31 @@
+package com.afirez.common;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.afirez.common.app.AppDelegate;
+
+/**
+ * Created by afirez on 2018/6/15.
+ */
+
+public class App extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        AppDelegate.INSTANCE.attachBaseContext(this, base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppDelegate.INSTANCE.onCreate(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        AppDelegate.INSTANCE.onTerminate(this);
+    }
+}
---
+++ lib-common/src/main/java/com/afirez/common/app/AppLifecycleCallbacks.java
@@ -0,0 +1,18 @@
+package com.afirez.common.app;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by afirez on 2018/6/15.
+ */
+
+public interface AppLifecycleCallbacks {
+
+    void attachBaseContext(Application app, Context base);
+
+    void onCreate(Application application);
+
+    void onTerminate(Application application);
+
+}
